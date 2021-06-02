79d12d0ad9c0dae7af22f81517ed8e9624ff9d7f
Gautitho <thomas.gautier38@orange.fr>
2021-06-02 18:21:04 +0200

    Client : js code splitted from html

---
+++ js/clientInteraction.js
@@ -0,0 +1,150 @@
+// Available states : LOCKED, IDLE, MOVE, SPELL, COMPANION
+var state = "LOCKED"; 
+var turn = "blue";
+var team = "blue";
+var spellsDataBase = {}
+var heroesDataBase = {}
+var entitiesDataBase = {}
+var selectedEntity = -1;
+var entitiesList = [];
+var myPlayer = {};
+var opPlayer = {};
+var path = [];
+
+$.getJSON("data/spells.json", function(data) {spellsDataBase = data});
+$.getJSON("data/entities.json", function(data) {entitiesDataBase = data});
+$.getJSON("data/heroes.json", function(data) {heroesDataBase = data});
+
+function updateState(newState)
+{
+    if (newState == "IDLE")
+    {
+        $("#stateBtn").css("background-color", "#00FF00");
+        $("#stateBtn").text("End turn");
+    }
+    else if (newState == "MOVE")
+    {
+        $("#stateBtn").css("background-color", "#0000FF");
+        $("#stateBtn").text("End move");
+    }
+    else
+    {
+        $("#stateBtn").css("background-color", "#FF0000");
+        $("#stateBtn").text("Opponent turn");
+    }
+    state = newState;
+}
+
+function updateBoard()
+{
+    for (y = 0; y < BOARD_ROWS; y++)
+    {
+        for (x = 0; x < BOARD_COLS; x++)
+        {
+            $("#board_" + x + "_" + y).css("background-color", "#FFFFFF");
+            $("#board_" + x + "_" + y).css("background-image", "");
+        }
+    }
+    for (i = 0; i < entitiesList.length; i++)
+    {
+        $("#board_" + entitiesList[i].x + "_" + entitiesList[i].y).css("background-image", "url(" + eval("entitiesDataBase." + entitiesList[i].descId + ".spritePath") + ")");
+    }
+}
+
+function updateMyStatus()
+{
+    $("#myStatusSprite").css("background-image", "url(" + eval("entitiesDataBase." + eval("heroesDataBase." + myPlayer.heroDescId + ".entityDescId") + ".spritePath") + ")");
+    $("#myStatusPv").text("PV : " + entitiesList[myPlayer.heroEntityId].pv + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + myPlayer.heroDescId + ".entityDescId") + ".pv"));
+    $("#myStatusAtk").text("ATK : " + entitiesList[myPlayer.heroEntityId].atk + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + myPlayer.heroDescId + ".entityDescId") + ".atk"));
+    $("#myStatusPm").text("PM : " + entitiesList[myPlayer.heroEntityId].pm + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + myPlayer.heroDescId + ".entityDescId") + ".pm"));
+}
+
+function updateOpStatus()
+{
+    $("#opStatusSprite").css("background-image", "url(" + eval("entitiesDataBase." + eval("heroesDataBase." + opPlayer.heroDescId + ".entityDescId") + ".spritePath") + ")");
+    $("#opStatusPv").text("PV : " + entitiesList[opPlayer.heroEntityId].pv + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + opPlayer.heroDescId + ".entityDescId") + ".pv"));
+    $("#opStatusAtk").text("ATK : " + entitiesList[opPlayer.heroEntityId].atk + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + opPlayer.heroDescId + ".entityDescId") + ".atk"));
+    $("#opStatusPm").text("PM : " + entitiesList[opPlayer.heroEntityId].pm + " / " + eval("entitiesDataBase." + eval("heroesDataBase." + opPlayer.heroDescId + ".entityDescId") + ".pm"));
+}
+
+function updateHandBar()
+{
+    $("#pa").text(myPlayer.pa);
+    $("#paStock").text(myPlayer.paStock);
+    for (i = 0; i < HAND_SPELLS; i++)
+    {
+        $("#spell_" + i).css("background-image", "");
+    }
+    for (j = 0; j < myPlayer.handSpellDescIds.length; j++)
+    {
+        $("#spell_" + j).css("background-image", eval("spellsDataBase." + myPlayer.handSpellDescIds[j] + ".spritePath"));
+    }
+}
+
+function stateBtnClick()
+{
+    if (state == "IDLE")
+    {
+        endTurn();
+    }
+    else if (state == "MOVE")
+    {
+        move();
+    }
+}
+
+function cancelBtnClick()
+{
+    if (state == "MOVE")
+    {
+        path = [];
+        selectedEntity = -1;
+        updateState("IDLE");
+        updateBoard();
+    }
+}
+
+function boardTileClick(tile)
+{
+    var x = tile.attr('id').split('_')[1];
+    var y = tile.attr('id').split('_')[2];
+
+    if (turn != team)
+    {
+        log("Not your turn, bro !");
+    }
+    else if (state == "IDLE")
+    {
+        for (i = 0; i < entitiesList.length; i++)
+        {
+            if (entitiesList[i].x == x && entitiesList[i].y == y && entitiesList[i].team == team)
+            {
+                updateState("MOVE");
+                selectedEntity = i;
+                path.push({"x" : x, "y" : y});
+                $("#board_" + x + "_" + y).css("background-color", "#6DB3F2");
+            } 
+        }
+    }
+    else if (state == "MOVE")
+    {
+        if (path[path.length-1].x != x || path[path.length-1].y != y)
+        {
+            path.push({"x" : x, "y" : y});
+            $("#board_" + x + "_" + y).css("background-color", "#6DB3F2");
+        }
+    }
+}
+
+function spellClick(spell)
+{
+    var spellId = spell.attr('id').split('_')[1];
+    console.log(spellId);
+}
+
+function log(message)
+{
+    $("#logs").append(`<p>${message}</p>`);
+    var element = document.getElementById("logs");
+    element.scrollTop = element.scrollHeight;
+}
---
+++ js/serverCom.js
@@ -0,0 +1,51 @@
+socket      = new WebSocket('ws://localhost:8000/');
+playerId    = Math.random().toString(36).substring(4);
+
+authCmd = {"cmd" : "AUTH", "playerId" : playerId};
+socket.onopen = function() {socket.send(JSON.stringify(authCmd));};
+
+socket.onmessage = function(handler)
+{
+    console.log(handler.data)
+    cmdObj = JSON.parse(handler.data)
+
+    if (cmdObj.cmd == "INIT")
+    {
+        team = cmdObj.team;
+    }
+    else if (cmdObj.cmd == "STATUS")
+    {
+        turn            = cmdObj.turn;
+        myPlayer        = cmdObj.myPlayer;
+        opPlayer        = cmdObj.opPlayer;
+        entitiesList    = cmdObj.entitiesList;
+
+        if (turn == team)
+        {
+            updateState("IDLE");
+        }
+        else
+        {
+            updateState("LOCKED");
+        }
+        updateMyStatus();
+        updateOpStatus();
+        updateHandBar();
+    }
+    updateBoard();
+};
+
+function endTurn()
+{
+    endTurnCmd = {"cmd" : "ENDTURN", "playerId" : playerId};
+    socket.send(JSON.stringify(endTurnCmd));
+}
+
+function move()
+{
+    moveCmd = {"cmd" : "MOVE", "playerId" : playerId, "entityId" : selectedEntity, "path" : path};
+    socket.send(JSON.stringify(moveCmd));
+    path = [];
+    selectedEntity = -1;
+    updateState("IDLE");
+}
