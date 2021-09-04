ba48654f95df5ad90b259a1c9dd454cc4181ffb6
Gautitho <thomas.gautier38@orange.fr>
2021-09-04 11:35:05 +0200

    Client : deck building page updated (WIP)

---
+++ docs/network.md
@@ -0,0 +1,4 @@
+Web server port internal : 80
+Web server port external : 3074
+Python server port internal : 50000
+Python server port external : 3724
--- pages/deckHero/deckHero.js
+++ pages/deckHero/deckHero.js
@@ -1,20 +1,199 @@
-socket          = new WebSocket('ws://localhost:50000/');
-socket.onopen   = function(){};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-socket.onmessage = function(handler)
+const PROJECT_ROOT_PATH = "../../"
+const HERO_CLASSES = 5
+const HERO_RACE_LIST = ['iop', 'xelor', 'cra']
+
+// Available states : HERO, SPELLS, COMPANIONS
+var state = "HERO"
+var heroesDataBase      = {}
+var entitiesDataBase    = {}
+var spellsDataBase      = {}
+var companionsDataBase  = {}
+
+$.when(getHeroesDataBase(), getEntitiesDataBase()).then(loadPage)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getHeroesDataBase()
 {
-    console.log(handler.data)
-    cmdObj = JSON.parse(handler.data)
+    return $.getJSON(PROJECT_ROOT_PATH + "data/heroes.json", function(data) {heroesDataBase = data});
+}
 
-    if (cmdObj.cmd == "ERROR")
+function getEntitiesDataBase()
+{
+    return $.getJSON(PROJECT_ROOT_PATH + "data/entities.json", function(data) {entitiesDataBase = data});
+}
+
+function getSpellsDataBase()
+{
+    return $.getJSON(PROJECT_ROOT_PATH + "data/spells.json", function(data) {spellsDataBase = data});
+}
+
+function getCompanionsDataBase()
+{
+    return $.getJSON(PROJECT_ROOT_PATH + "data/companions.json", function(data) {companionsDataBase = data});
+}
+
+function loadPage()
+{
+    cleanPage()
+    if (state == "HERO")
     {
-        errorLog(cmdObj.msg);
+        $("#heroBtn").css("background-color", "#FF0000");
+        $("#spellsBtn").css("background-color", "#0000FF");
+        $("#companionsBtn").css("background-color", "#0000FF");
+        heroChoice();
+    }
+    else if (state == "SPELLS")
+    {
+        $("#heroBtn").css("background-color", "#0000FF");
+        $("#spellsBtn").css("background-color", "#FF0000");
+        $("#companionsBtn").css("background-color", "#0000FF");
+        spellsChoice();
+    }
+    else if (state == "COMPANIONS")
+    {
+        $("#heroBtn").css("background-color", "#0000FF");
+        $("#spellsBtn").css("background-color", "#0000FF");
+        $("#companionsBtn").css("background-color", "#FF0000");
+        companionsChoice();
     }
 }
 
-function errorLog(message)
+function cleanPage()
 {
-    $("#errorLog").text(message);
+    document.getElementById("choiceLayout").removeChild(document.getElementById("choiceGrid"));
+}
+
+function heroChoice()
+{
+    document.getElementById("choiceLayout").insertAdjacentHTML('beforeend',`<div id="choiceGrid"></div>`);
+    $("#choiceGrid").addClass("flex-grow grid grid-cols-" + String(HERO_RACE_LIST.length) + " grid-rows-" + String(1 + HERO_CLASSES))
+
+    for (y = 0; y < 1 + HERO_CLASSES; y++)
+    {
+        for (x = 0; x < HERO_RACE_LIST.length; x++)
+        {
+            if (y == 0)
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}"></div>`);
+                $("#choiceGrid_" + x).text(HERO_RACE_LIST[x]);
+            }
+            else
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}_${y}" class="border-2 border-light-blue-500 border-opacity-25 relative bg-center bg-contain bg-no-repeat"></div>`);
+                $("#choiceGrid_" + x + "_" + y).click(function() {heroClick($(this))});
+            }
+        } 
+    }
+
+    for (x = 0; x < HERO_RACE_LIST.length; x++)
+    {
+        var y = 1;
+        for (h in heroesDataBase)
+        {
+            if (heroesDataBase[h]["race"] == HERO_RACE_LIST[x])
+            {
+                $("#choiceGrid_" + x + "_" + y).css("background-image", "url(" + PROJECT_ROOT_PATH + eval("entitiesDataBase." + eval("heroesDataBase." + h + ".entityDescId") + ".descSpritePath") + ")");
+                y = y + 1;
+            }
+        }
+    }
+}
+
+function spellsChoice()
+{
+    document.getElementById("choiceLayout").insertAdjacentHTML('beforeend',`<div id="choiceGrid"></div>`);
+    $("#choiceGrid").addClass("flex-grow grid grid-cols-" + String(HERO_RACE_LIST.length) + " grid-rows-" + String(1 + HERO_CLASSES))
+
+    for (y = 0; y < 1 + HERO_CLASSES; y++)
+    {
+        for (x = 0; x < HERO_RACE_LIST.length; x++)
+        {
+            if (y == 0)
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}"></div>`);
+                $("#choiceGrid_" + x).text(HERO_RACE_LIST[x]);
+            }
+            else
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}_${y}" class="border-2 border-light-blue-500 border-opacity-25 relative bg-center bg-contain bg-no-repeat"></div>`);
+                $("#choiceGrid_" + x + "_" + y).click(function() {heroClick($(this))});
+            }
+        } 
+    }
+
+    for (x = 0; x < HERO_RACE_LIST.length; x++)
+    {
+        var y = 1;
+        for (h in heroesDataBase)
+        {
+            if (heroesDataBase[h]["race"] == HERO_RACE_LIST[x])
+            {
+                $("#choiceGrid_" + x + "_" + y).css("background-image", "url(" + PROJECT_ROOT_PATH + eval("entitiesDataBase." + eval("heroesDataBase." + h + ".entityDescId") + ".descSpritePath") + ")");
+                y = y + 1;
+            }
+        }
+    }
+}
+
+function companionsChoice()
+{
+    document.getElementById("choiceLayout").insertAdjacentHTML('beforeend',`<div id="choiceGrid"></div>`);
+    $("#choiceGrid").addClass("flex-grow grid grid-cols-" + String(HERO_RACE_LIST.length) + " grid-rows-" + String(1 + HERO_CLASSES))
+
+    for (y = 0; y < 1 + HERO_CLASSES; y++)
+    {
+        for (x = 0; x < HERO_RACE_LIST.length; x++)
+        {
+            if (y == 0)
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}"></div>`);
+                $("#choiceGrid_" + x).text(HERO_RACE_LIST[x]);
+            }
+            else
+            {
+                document.getElementById("choiceGrid").insertAdjacentHTML('beforeend',`<div id="choiceGrid_${x}_${y}" class="border-2 border-light-blue-500 border-opacity-25 relative bg-center bg-contain bg-no-repeat"></div>`);
+                $("#choiceGrid_" + x + "_" + y).click(function() {heroClick($(this))});
+            }
+        } 
+    }
+
+    for (x = 0; x < HERO_RACE_LIST.length; x++)
+    {
+        var y = 1;
+        for (h in heroesDataBase)
+        {
+            if (heroesDataBase[h]["race"] == HERO_RACE_LIST[x])
+            {
+                $("#choiceGrid_" + x + "_" + y).css("background-image", "url(" + PROJECT_ROOT_PATH + eval("entitiesDataBase." + eval("heroesDataBase." + h + ".entityDescId") + ".descSpritePath") + ")");
+                y = y + 1;
+            }
+        }
+    }
+}
+
+function heroChoiceBtnClick()
+{
+    state = "HERO";
+    loadPage();
+}
+
+function spellsChoiceBtnBtnClick()
+{
+    state = "SPELLS";
+    loadPage();
+}
+
+function companionsChoiceBtnClick()
+{
+    state = "COMPANIONS";
+    loadPage();
 }
 
 function heroClick()
